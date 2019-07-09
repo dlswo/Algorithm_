c72adcd3012d606ba074beb48eddd6a9db1c04a9
dlswo <dlswo4456@naver.com>
2019-07-09 18:01:20 +0900

    StringBuffer reverse!!

---
+++ src/ReverseInt.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class ReverseInt {
+	
+//	자연수 뒤집어 배열로 만들기
+//	문제 설명
+//	자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+	public static void main(String[] args) {
+		long n = 12345;
+		
+		String reverse = new StringBuffer(String.valueOf(n)).reverse().toString();
+		int[] answer = new int[reverse.length()];
+		for(int i = 0; i < reverse.length(); i++){
+			answer[i] = Integer.parseInt(reverse.substring(i, i+1));
+		}
+		System.out.println(Arrays.toString(answer));
+		
+	}
+}
