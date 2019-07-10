472597a7c36cc681636a5e96c4ccd2824e75f39d
dlswo <dlswo4456@naver.com>
2019-07-10 09:54:27 +0900

    Math.pow 제곱, Math.sqrt 제곱근, Math.floor !Double.isInfinite 정수인지판별

--- src/SquareRoot.java
+++ src/SquareRoot.java
@@ -5,14 +5,14 @@ public class SquareRoot {
 //    임의의 정수 n에 대해, n이 어떤 정수 x의 제곱인지 아닌지 판단하려 합니다.
 //    n이 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
     public static void main(String[] args) {
-        long n = 120;
+        long n = 121;
         long answer = 0;
         double i =  Math.sqrt(n);
-        if( i * i == n){
-            answer = (long)i + 1;
+        if((i == Math.floor(i)) && !Double.isInfinite(i)){
+        	answer =  (long)Math.pow(i + 1, 2);
         }else{
-            answer = -1;
+        	answer = -1;
         }
-        System.out.println(i * i);
+        System.out.println(answer);
     }
 }
