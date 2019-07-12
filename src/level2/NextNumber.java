98320c76e08a9c80be201eae350618dc1968f439
dlswo <dlswo4456@naver.com>
2019-07-12 14:38:22 +0900

    어려웡~

--- src/level2/NextNumber.java
+++ src/level2/NextNumber.java
@@ -1,8 +1,5 @@
 package level2;
 
-import java.nio.ByteBuffer;
-import java.util.Arrays;
-
 public class NextNumber {
 	
 //	다음 큰 숫자
@@ -17,16 +14,30 @@ public class NextNumber {
 //	자연수 n이 매개변수로 주어질 때, n의 다음 큰 숫자를 return 하는 solution 함수를 완성해주세요.
 	public static void main(String[] args) {
 		
-		int n = 78;
+		int n = 15;
+		int answer = 0;
+		int count = getCount(n++);
+		int answerCnt = 0;
 		
-		String strN = "1";
-		while(n != 1){
-			strN += n % 2;
+		while(answerCnt != count){
+			answer = n;
+			answerCnt = getCount(n++);
+		}
+		
+		System.out.println(answer);
+		
+	}
+	
+	public static int getCount(int n){
+		int count = 0;
+		
+		while(n >= 1){
+			if(n % 2 == 1)
+				count++;
 			n = n / 2;
 		}
 		
-		System.out.println(strN);
-		
+		return count;
 	}
 
 }
