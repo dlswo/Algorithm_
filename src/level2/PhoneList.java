7f8c6d58ecdc301b9430addcdc78dea21348d4fb
dlswo <dlswo4456@naver.com>
2019-07-26 13:46:07 +0900

    commit!

--- src/level2/PhoneList.java
+++ src/level2/PhoneList.java
@@ -1,5 +1,6 @@
 package level2;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class PhoneList {
@@ -18,13 +19,27 @@ public class PhoneList {
 		String[] phone_book = {"119", "97674223", "1195524421"};
 		boolean answer = true;
 		
-		// 해쉬맵을 선언
-		HashMap<String, Integer> hm = new HashMap<>();
-		
-		// 배열을 hm에 넣고 같은게 있으면 false
+/*		// 가장 크기가 작은 length를 구한다
+		int minLen = phone_book[0].length();
+
 		for(int i = 0; i < phone_book.length; i++){
-			hm.put(phone_book[i], i);
+			if(minLen > phone_book[i].length()){
+				minLen = phone_book[i].length(); 
+			}
+		}*/
+		
+		//같으것이 있으면 false		
+		for(int i = 0; i < phone_book.length; i++){
+			for(int j = i + 1; j < phone_book.length; j++){
+				if(phone_book[i].startsWith(phone_book[j])){
+					answer = false;
+				}
+				if(phone_book[j].startsWith(phone_book[i])){
+					answer = false;
+				}
+			}
 		}
+		System.out.println(answer);
 		
 	}
 }
