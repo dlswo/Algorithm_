4bf37a61c201e684c344ef0ecb44b005c588b2ff
dlswo <dlswo4456@naver.com>
2019-08-06 11:22:27 +0900

    뭔가 이상해

--- src/level2/FindPrimeNum.java
+++ src/level2/FindPrimeNum.java
@@ -11,6 +11,7 @@ public class FindPrimeNum {
 //
 //	각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
 	static Map<Integer, Integer> hash = new HashMap<>(); // 중복제거를 위해 전역으로..
+	
 	public static void main(String[] args) {
 		String numbers = "011";
 		int answer = 0;
